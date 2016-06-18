a274b594396af619bf886e548064f48359c4e4bc
Julian Cook <juliantcook@gmail.com>
2016-06-18 16:36:22 +0100

    Output dot file and generate png during test

--- src/test/groovy/uk/julianc/groovis/DependencyGraphSpec.groovy
+++ src/test/groovy/uk/julianc/groovis/DependencyGraphSpec.groovy
@@ -7,6 +7,22 @@ import static org.codehaus.groovy.control.CompilePhase.CONVERSION
 
 class DependencyGraphSpec extends Specification {
 
+    def cleanup() {
+        def file = writeDotFile()
+        generateImage(file)
+    }
+
+    private File writeDotFile() {
+        def dir = new File("build/graphs/")
+        dir.mkdirs()
+        def fileName = "${specificationContext.currentFeature.name.replace(' ', '_')}.dot"
+        new File(dir, fileName) << GroovisBuilder.instance.generate()
+    }
+
+    private generateImage(File file) {
+        "dot -O -T png ${file.path}".execute()
+    }
+
     def 'single class with no fields produces single node'() {
         given:
         def transform = new GroovisTransform()
